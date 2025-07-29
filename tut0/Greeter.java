33e8e51688f9f966f2b9835292c717e9dfdb9183
Aphelele Makhanya Zenzile <27403122@sun.ac.za>
2025-07-29 12:25:41 +0200

    cleaning this ish up ..

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class Greeter {
 	public static void main(String args[]) {
-		String name = getName();
-		System.out.println("The retrieved name is: " + name);
 	}
 
 	public static String getName() {
