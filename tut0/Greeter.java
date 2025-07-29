ee7031a959275da0c7a495df61ee63824d1ddfab
Aphelele Makhanya Zenzile <27403122@sun.ac.za>
2025-07-29 11:54:29 +0200

    Added a greeter function that takes a command line arg [#6]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,10 @@
 public class Greeter {
 	public static void main(String args[]) {
-		
+		String myName = args[0];
+		greeter(myName);
+	}
+
+	public static void greeter(String name) {
+		System.out.println("I am greeting this guy, the guy is called " + name);
 	}
 }
