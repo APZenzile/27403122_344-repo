5ed4dd930c009451c561d6696b201c7b518ba715
Aphelele Makhanya Zenzile <27403122@sun.ac.za>
2025-07-29 12:40:04 +0200

    added javadoc, sorry manager [#16]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,10 +1,23 @@
 import java.util.*;
 
+/**
+ * this class is a greeting class
+ * and provides various ways of greeting.
+ */
 public class Greeter {
+
+	/**
+	 * and this is a standard main java function.
+	 * @param args
+	 */
 	public static void main(String args[]) {
 		greeter();
 	}
 
+	/**
+	 * This function retrieves a name from the command line and 
+	 * @return returns the name.
+	 */
 	public static String getName() {
 		Scanner scanner = new Scanner(System.in);
 		String name = scanner.nextLine();
@@ -12,6 +25,9 @@ public class Greeter {
 		return name;
 	}
 
+	/**
+	 * this function is greeting function.
+	 */
 	public static void greeter() {
 		Scanner scanner = new Scanner(System.in);
 		String name = scanner.nextLine();
